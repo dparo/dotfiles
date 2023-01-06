0a38f09be4a67c80337a40d93c146a3a1beb6888
Davide Paro <dparo@outlook.it>
2023-01-06 15:05:10 +0100

    More binds

---
+++ roles/xdg/files/bin/i3-postman.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+FILTER='instance="postman" class="Postman"'
+
+i3-msg "[ $FILTER ] scratchpad show, move position center" ||
+    i3-msg "[ $FILTER ] focus" ||
+    exec ~/opt/Postman/Postman
