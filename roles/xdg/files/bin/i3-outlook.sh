aaa0d600cdbd8a7d99ac83596d17412303f449d3
Davide Paro <dparo@outlook.it>
2023-01-06 14:30:03 +0100

    More fixes

--- roles/xdg/files/bin/i3-outlook.sh
+++ roles/xdg/files/bin/i3-outlook.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-#
 
-FILTER='instance="crx__pkooggnaalmfkidjmlhoelhdllpphaga" class="Microsoft-edge"'
+FILTER='instance="crx_pkooggnaalmfkidjmlhoelhdllpphaga" class="Google-chrome"'
 
 i3-msg "[ $FILTER ] scratchpad show, move position center" ||
     i3-msg "[ $FILTER ] focus" ||
-    gtk-launch msedge-pkooggnaalmfkidjmlhoelhdllpphaga-Default.desktop
+    gtk-launch chrome-pkooggnaalmfkidjmlhoelhdllpphaga-Default.desktop
