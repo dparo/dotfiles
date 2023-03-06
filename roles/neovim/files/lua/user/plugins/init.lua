032de2c985ad6632bebbc4f4ea83898724fca080
Davide Paro <d.paro@reply.it>
2023-03-06 12:54:41 +0100

    More neovim fixes

--- roles/neovim/files/lua/user/plugins/init.lua
+++ roles/neovim/files/lua/user/plugins/init.lua
@@ -30,7 +30,7 @@ local generic = {
         "cappyzawa/trim.nvim",
         config = function()
             require("trim").setup {
-                disable = {},
+                ft_blocklist = {},
                 patterns = {
                     [[%s/\s\+$//e]], -- remove unwanted spaces
                     [[%s/\($\n\s*\)\+\%$//]], -- trim last line
@@ -750,8 +750,6 @@ local themes = {
                 dimInactive = false, -- dim inactive window `:h hl-NormalNC`
                 globalStatus = false, -- adjust window separators highlight for laststatus=3
                 terminalColors = true, -- define vim.g.terminal_color_{0,17}
-                colors = {},
-                overrides = {},
                 theme = "default", -- Load "default" theme or the experimental "light" theme
             }
         end,
