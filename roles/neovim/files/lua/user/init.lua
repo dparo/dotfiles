23d7a5ce0039f3c56f8ec8b10ec4c98d0334f1b7
Davide Paro <d.paro@reply.it>
2023-03-25 09:47:54 +0100

    Minor fixes

--- roles/neovim/files/lua/user/init.lua
+++ roles/neovim/files/lua/user/init.lua
@@ -4,7 +4,34 @@ _G.user = M
 M.utils = require "user.utils"
 require "user.options"
 require "user.mappings"
-require "user.plugins"
+
+-- Setup lazy.nvim
+local running_headless = next(vim.api.nvim_list_uis()) == nil -- If dictionaries of UIs is empty => headless mode
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not running_headless and not vim.loop.fs_stat(lazypath) then
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
+    print "Cloning lazy.nvim .."
+
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
+
+    vim.fn.system {
+        "rm",
+        "-rf",
+        vim.env.HOME .. "/.config/nvim/plugin/packer_compiled.lua",
+    }
+end
+
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(require("user.plugins"), {})
 require "user.theme"
 require "user.autocommands"
 require "user.abbreviations"
--- roles/neovim/files/lua/user/plugins/init.lua
+++ roles/neovim/files/lua/user/plugins/init.lua
@@ -397,7 +397,7 @@ local generic = {
         "nvim-treesitter/nvim-treesitter",
         branch = "master",
         build = function()
-            vim.cmd [[:TSUpdateSync all]]
+            vim.cmd [[:TSUpdate]]
         end,
         dependencies = { "nvim-treesitter/playground", "p00f/nvim-ts-rainbow", "nvim-treesitter/nvim-treesitter-context" },
         config = function()
@@ -803,32 +803,5 @@ vim.list_extend(plugins, vcs_integration)
 vim.list_extend(plugins, language_specific_toolings)
 
 
--- Setup lazy.nvim
 
-local running_headless = next(vim.api.nvim_list_uis()) == nil -- If dictionaries of UIs is empty => headless mode
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not running_headless and not vim.loop.fs_stat(lazypath) then
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-    print "Cloning lazy.nvim .."
-
-    vim.fn.system {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    }
-
-    vim.fn.system {
-        "rm",
-        "-rf",
-        vim.env.HOME .. "/.config/nvim/plugin/packer_compiled.lua",
-    }
-end
-
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins, {})
 return plugins
