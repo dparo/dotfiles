e4be80ec53872b3ee02cbabcef7b0e82999a07e5
Davide Paro <dparo@outlook.it>
2023-05-18 07:40:53 +0200

    More shell skeleton

--- roles/neovim/files/skeletons/sh.sh
+++ roles/neovim/files/skeletons/sh.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-SELF_BASH_SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
+if builtin type readlink 1>/dev/null 2>/dev/null; then
+    # Linux
+    SELF_BASH_SCRIPT_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+else
+    # Mac OS
+    SELF_BASH_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+fi
 
+SELF_BASH_SCRIPT_EXE="$SELF_BASH_SCRIPT_DIR/$(basename "${BASH_SOURCE[0]}")"
 cd "$SELF_BASH_SCRIPT_DIR" || exit 1
+
+
+main() {
+    echo "$SELF_BASH_SCRIPT_DIR"
+    echo "$SELF_BASH_SCRIPT_EXE"
+}
+
+set -x
+set -e
+main "$@"
