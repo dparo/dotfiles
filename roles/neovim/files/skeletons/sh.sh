ea1672523e8720eab0126b0849845670356224ae
Davide Paro <dparo@outlook.it>
2023-05-18 07:45:25 +0200

    Modified shell skeleton

--- roles/neovim/files/skeletons/sh.sh
+++ roles/neovim/files/skeletons/sh.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+
 if builtin type readlink 1>/dev/null 2>/dev/null; then
     # Linux
     SELF_BASH_SCRIPT_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
@@ -9,11 +10,13 @@ else
     SELF_BASH_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 fi
 
+# shellcheck disable=SC2034
 SELF_BASH_SCRIPT_EXE="$SELF_BASH_SCRIPT_DIR/$(basename "${BASH_SOURCE[0]}")"
-cd "$SELF_BASH_SCRIPT_DIR" || exit 1
 
 
 main() {
+    cd "$SELF_BASH_SCRIPT_DIR" || exit 1
+
     echo "$SELF_BASH_SCRIPT_DIR"
     echo "$SELF_BASH_SCRIPT_EXE"
 }
