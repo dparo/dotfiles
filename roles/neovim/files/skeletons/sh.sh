b9daa61204b8bf331df6785767e140a65607b43f
Davide Paro <dparo@outlook.it>
2023-05-18 07:58:03 +0200

    Modified shell skeleton

--- roles/neovim/files/skeletons/sh.sh
+++ roles/neovim/files/skeletons/sh.sh
@@ -3,22 +3,18 @@
 
 
 if builtin type readlink 1>/dev/null 2>/dev/null; then
-    # Linux
+    # shellcheck disable=SC2034
     SELF_BASH_SCRIPT_DIR="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 else
-    # Mac OS
+    # shellcheck disable=SC2034
     SELF_BASH_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 fi
-
-# shellcheck disable=SC2034
-SELF_BASH_SCRIPT_EXE="$SELF_BASH_SCRIPT_DIR/$(basename "${BASH_SOURCE[0]}")"
 
 
 main() {
     cd "$SELF_BASH_SCRIPT_DIR" || exit 1
 
-    echo "$SELF_BASH_SCRIPT_DIR"
-    echo "$SELF_BASH_SCRIPT_EXE"
+    echo "Hello world"
 }
 
 set -x
