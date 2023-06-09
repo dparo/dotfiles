dc63b202efb76c314ed795192dbfd0d83b85eb0b
Davide Paro <d.paro@reply.it>
2023-06-09 10:27:59 +0200

    NullLs setup semgrep

--- roles/neovim/files/lua/plugins/lsp.lua
+++ roles/neovim/files/lua/plugins/lsp.lua
@@ -133,6 +133,7 @@ return {
                     },
 
                     null_ls.builtins.formatting.google_java_format,
+                    null_ls.builtins.diagnostics.semgrep,
 
                     null_ls.builtins.formatting.prettier.with {
                         filetypes = {
