f92a3b454325c54c699ddb886647690422328dd5
Davide Paro <dparo@outlook.it>
2023-06-18 11:37:26 +0200

    Modified screenshot program

--- roles/xdg/files/bin/screenshot.sh
+++ roles/xdg/files/bin/screenshot.sh
@@ -3,8 +3,14 @@
 set -e
 
 mkdir -p "$HOME/Pictures/Screenshots/"
-location="$HOME/Pictures/Screenshots/Screenshot$(date +%Y-%m-%d_%H:%M:%S).png"
-maim -m 9 -s "$location"
-mpv --keep-open=no /usr/share/sounds/freedesktop/stereo/screen-capture.oga 1> /dev/null 2> /dev/null &
-xclip -selection clipboard -t "image/png" < "$location"
-dunstify -i camera-photo-symbolic "Screenshot taken" "Screenshot saved into clipboard!"
+
+if builtin type flameshot 1>/dev/null 2>/dev/null; then
+    flameshot gui -p "$HOME/Pictures/Screenshots" -c
+    # mpv --keep-open=no /usr/share/sounds/freedesktop/stereo/screen-capture.oga 1> /dev/null 2> /dev/null
+else
+    location="$HOME/Pictures/Screenshots/Screenshot_$(date +%Y-%m-%d_%H:%M:%S).png"
+    maim -m 9 -s "$location"
+    # mpv --keep-open=no /usr/share/sounds/freedesktop/stereo/screen-capture.oga 1> /dev/null 2> /dev/null &
+    xclip -selection clipboard -t "image/png" < "$location"
+    dunstify -i camera-photo-symbolic "Screenshot taken" "Screenshot saved into clipboard!"
+fi
