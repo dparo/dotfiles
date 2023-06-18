70cf05fd503f5fd89f7e2fe6039039c56d54fffa
Davide Paro <dparo@outlook.it>
2023-06-18 11:22:02 +0200

    Modified screenshot program

--- roles/xdg/files/bin/screenshot.sh
+++ roles/xdg/files/bin/screenshot.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
-location=$HOME/Pictures/Screenshot$(date +%Y-%m-%d_%H:%M:%S).png
+mkdir -p "$HOME/Pictures/Screenshots/"
+location="$HOME/Pictures/Screenshots/Screenshot$(date +%Y-%m-%d_%H:%M:%S).png"
 maim -m 9 -s "$location"
 mpv --keep-open=no /usr/share/sounds/freedesktop/stereo/screen-capture.oga 1> /dev/null 2> /dev/null &
 xclip -selection clipboard -t "image/png" < "$location"
 dunstify -i camera-photo-symbolic "Screenshot taken" "Screenshot saved into clipboard!"
-rm -rf "$location"
