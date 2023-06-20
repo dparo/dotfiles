be4d506765b2498acd1de9b78a11edc469343ac4
Davide Paro <dparo@outlook.it>
2023-06-20 07:30:55 +0200

    Modified sway wrapper script

--- roles/xdg/files/bin/sway-wrapper.sh
+++ roles/xdg/files/bin/sway-wrapper.sh
@@ -13,6 +13,7 @@ export GBM_BACKEND=nvidia-drm
 export QT_QPA_PLATFORM=wayland
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export GDK_BACKEND=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
 
 export WLR_NO_HARDWARE_CURSORS=1
 export WLR_BACKEND=vulkan
@@ -24,4 +25,4 @@ export __GLX_VENDOR_LIBRARY_NAME=nvidia
 export __GL_GSYNC_ALLOWED=0
 export __GL_VRR_ALLOWED=0
 
-sway --unsupported-gpu
+exec sway --unsupported-gpu
