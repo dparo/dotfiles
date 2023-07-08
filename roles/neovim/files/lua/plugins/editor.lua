a274a6f5a239dad2388e382a18e7762c523ab335
Davide Paro <dparo@outlook.it>
2023-07-08 10:29:12 +0200

    Modified indent-blankline stuff

--- roles/neovim/files/lua/plugins/editor.lua
+++ roles/neovim/files/lua/plugins/editor.lua
@@ -75,9 +75,8 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         config = function()
             require("indent_blankline").setup {
-                -- for example, context is off by default, use this to turn it on
                 show_current_context = true,
-                show_current_context_start = true,
+                show_current_context_start = false,
                 filetype_exclude = {
                     "lspinfo",
                     "checkhealth",
