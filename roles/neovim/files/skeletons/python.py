489785ae52fe51156b61dbd112c9e4520e64c379
Davide Paro <d.paro@reply.it>
2023-07-10 16:48:20 +0200

    Changed python skeleton

--- roles/neovim/files/skeletons/python.py
+++ roles/neovim/files/skeletons/python.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 
-def main(args: list[str]):
-    # TODO
+def main(args: argparse.Namespace):
     pass
 
-if __name__ == '__main__':
-    import os
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    import sys
-    main(sys.argv)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog="my-prog.py",
+        description="My description",
+        epilog="",
+    )
+
+    parser.add_argument("input_file", type=str, help="The input file")
+    parser.add_argument("-o", "--output", type=str, help="The output file", required=True)
+    args = parser.parse_args()
+    main(args)
