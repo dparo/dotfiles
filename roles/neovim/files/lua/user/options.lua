2b00b5c263cd4c506e36325d984eea7024328ae7
Davide Paro <dparo@outlook.it>
2023-07-29 17:56:15 +0200

    Modified netrw config

--- roles/neovim/files/lua/user/options.lua
+++ roles/neovim/files/lua/user/options.lua
@@ -1,3 +1,9 @@
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 0
+vim.g.netrw_altv = 1
+vim.g.netrw_liststyle = 3
+
+
 vim.o.background = "dark"
 vim.o.guifont = "JetBrainsMono Nerd Font:h10.0"
 
