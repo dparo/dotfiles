25a9b20b7507cbc3d307c63572cf3700ea6365cb
Davide Paro <dparo@outlook.it>
2023-08-10 17:10:08 +0200

    Disable indent-blankline

--- roles/neovim/files/lua/plugins/editor.lua
+++ roles/neovim/files/lua/plugins/editor.lua
@@ -72,6 +72,7 @@ return {
 
     {
         "lukas-reineke/indent-blankline.nvim",
+        enabled = false,
         config = function()
             require("indent_blankline").setup {
                 show_current_context = true,
