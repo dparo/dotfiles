002dfc038f99804f36428c787f1b5b0bd9fb05f5
Davide Paro <dparo@outlook.it>
2023-08-16 07:43:30 +0200

    nvim: Enable netrw

--- roles/neovim/files/lua/user/options.lua
+++ roles/neovim/files/lua/user/options.lua
@@ -1,6 +1,6 @@
 -- Disable netrw. We hijack netrw using nvim-tree plugin
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 0
