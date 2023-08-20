a25c86b3d19625a4ab4ccd40537b6f7c5dead3dd
Davide Paro <dparo@outlook.it>
2023-08-20 12:38:36 +0200

    Minor fix

--- roles/xdg/files/bin/asciidoctor-pdf-watch.sh
+++ roles/xdg/files/bin/asciidoctor-pdf-watch.sh
@@ -4,6 +4,6 @@
 while true; do
     find ./ -type f -name 'Makefile' -o -name "*.adoc" -o -name "*.png" -o -name ".svg" -o -name "*.jpg" -o -name "*.jpeg" \
         ! -path "./.git/*" ! -path "node_modules/*" |
-        entr -r -d asciidoctor-pdf -a allow-uri-read "$1"
+        entr -r -d asciidoctor -a allow-uri-read -r asciidoctor-pdf -b pdf "$1"
     sleep 0.5
 done
