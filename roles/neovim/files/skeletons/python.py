30c74baa08b3792cd1064f0697c96f346ac12707
Davide Paro <dparo@outlook.it>
2023-09-01 07:32:49 +0200

    Updated python skeleton

--- roles/neovim/files/skeletons/python.py
+++ roles/neovim/files/skeletons/python.py
@@ -3,18 +3,32 @@
 
 import argparse
 
-def main(args: argparse.Namespace):
-    pass
 
-
-if __name__ == '__main__':
+def parse_cmdline_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="my-prog.py",
         description="My description",
         epilog="",
     )
 
-    parser.add_argument("input_file", type=str, help="The input file")
-    parser.add_argument("-o", "--output", type=str, help="The output file", required=True)
-    args = parser.parse_args()
+    parser.add_argument(
+        "input_file", type=argparse.FileType("r"), help="The input file"
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=argparse.FileType("w"),
+        help="The output file",
+        required=True,
+    )
+    return parser.parse_args()
+
+
+def main(args_namespace: argparse.Namespace):
+    args = args_namespace.vars()
+    print(args)
+
+
+if __name__ == "__main__":
+    args = parse_cmdline_args()
     main(args)
