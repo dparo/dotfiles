bd1365c59f4c3010c702399b9fb92d820907b1c6
Davide Paro <dparo@outlook.it>
2023-09-13 22:22:27 +0200

    Better python skeleton

--- roles/neovim/files/skeletons/python.py
+++ roles/neovim/files/skeletons/python.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import sys
 import io
+import logging
+import sys
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def parse_cmdline_args() -> argparse.Namespace:
@@ -12,33 +17,84 @@ def parse_cmdline_args() -> argparse.Namespace:
         description="My description",
         epilog="",
     )
+
+    ##
+    ## Positional args
+    ##
     parser.add_argument(
-        "-i",
-        "--input_file",
-          type=argparse.FileType("r"),
+        "input_file",
+        type=argparse.FileType(mode="r", encoding="utf-8"),
+        nargs="?",
         help="The input file",
-        required = False,
         default=sys.stdin,
+    )
+
+    ##
+    ## Special arguments
+    ##
+    parser.add_argument(
+        "--log",
+        type=str.upper,
+        dest="log_level",
+        nargs="?",
+        choices=[
+            "DEBUG",
+            "INFO",
+            "WARN",
+            "WARNING",
+            "ERROR",
+            "CRITICAL",
+        ],
+        help="Logging level",
+        default="WARNING",
+    )
+
+    parser.add_argument(
+        "--log-file",
+        type=str,
+        dest="log_file",
+        nargs="?",
+        help="Log file",
+        default=None,
     )
 
     parser.add_argument(
         "-o",
         "--output",
-        type=argparse.FileType("w"),
+        type=argparse.FileType(mode="w", encoding="utf-8"),
+        dest="output_file",
+        nargs="?",
         help="The output file",
-        default=sys.stdout
+        default=sys.stdout,
     )
+
     return parser.parse_args(sys.argv[1:])
 
 
 def main(args: argparse.Namespace) -> None:
     input_file: io.TextIOWrapper = args.input_file
-    output_file: io.TextIOWrapper = args.output
+    output_file: io.TextIOWrapper = args.output_file
 
-    print(args)
-    print(input_file.name)
-    print(output_file.name)
+    ## Echo
+    print(input_file.read(), file=output_file)
+
+
+def setup_logging(args: argparse.Namespace):
+    logging_handlers: list[logging.Handler] = [logging.StreamHandler()]
+    if args.log_file:
+        logging_handlers.append(
+            logging.FileHandler(args.log_file, mode="w", encoding="utf-8")
+        )
+    logging.basicConfig(
+        handlers=logging_handlers,
+        format="%(asctime)s - %(filename)s:%(lineno)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z",
+        level=getattr(logging, args.log_level.upper()),
+    )
+    logging.info(f"Command line args namespace: {args}")
 
 
 if __name__ == "__main__":
-    main(args=parse_cmdline_args())
+    args = parse_cmdline_args()
+    setup_logging(args=args)
+    main(args=args)
