0086c132d8bcc9177311615837a9f76f634a3b0f
Davide Paro <dparo@outlook.it>
2024-01-03 20:24:40 +0100

    Fix dropdown terminal

--- roles/xdg/files/bin/i3-st-dropdown.sh
+++ roles/xdg/files/bin/i3-st-dropdown.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-FILTER='instance="st-256color" class="st-256color" title="^dropdown.*"'
+# FILTER='instance="kitty" class="^dropdown-term$"'
+FILTER='instance="st-256color" class="^dropdown-term$"'
 
+# kitty --name "kitty" --class "dropdown-term" -e tmux new-session -A -s dropdown -c "$HOME"
 i3-msg "[ $FILTER ] scratchpad show, move position center" ||
     i3-msg "[ $FILTER ] move workspace current, focus" ||
-    st -g 200x60 -e tmux new-session -A -s dropdown -c "$HOME"
+    st -c "dropdown-term" -g 200x60 -e tmux new-session -A -s dropdown -c "$HOME"
 
