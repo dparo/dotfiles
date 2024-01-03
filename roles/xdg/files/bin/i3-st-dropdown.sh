1a10b9b689e9cf1fb2162ee34afe80cc2fb0fadd
Davide Paro <dparo@outlook.it>
2024-01-03 18:30:52 +0100

    feat: i3-st-dropdown terminal support

--- roles/xdg/files/bin/i3-st-dropdown.sh
+++ roles/xdg/files/bin/i3-st-dropdown.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-FILTER='instance="st-256color" class="st-256color" title="^dropdown@.*"'
+FILTER='instance="st-256color" class="st-256color" title="^dropdown.*"'
 
-i3-msg "[ $FILTER ] scratchpad show, resize set 1024 640, move position center" ||
-    i3-msg "[ $FILTER ] focus" ||
-    st -e tmux new-session -A -s dropdown -c "$HOME"
+i3-msg "[ $FILTER ] scratchpad show, move position center" ||
+    i3-msg "[ $FILTER ] move workspace current, focus" ||
+    st -g 200x60 -e tmux new-session -A -s dropdown -c "$HOME"
 
