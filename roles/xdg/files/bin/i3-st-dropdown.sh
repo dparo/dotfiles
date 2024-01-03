3d114fb1a54b79f8f5143133dcab2867d17de510
Davide Paro <dparo@outlook.it>
2024-01-03 17:28:56 +0100

    Fix st terminal size

--- roles/xdg/files/bin/i3-st-dropdown.sh
+++ roles/xdg/files/bin/i3-st-dropdown.sh
@@ -3,7 +3,7 @@
 
 FILTER='instance="st-256color" class="st-256color" title="^dropdown@.*"'
 
-i3-msg "[ $FILTER ] scratchpad show, resize set 640 480, move position center" ||
+i3-msg "[ $FILTER ] scratchpad show, resize set 1024 640, move position center" ||
     i3-msg "[ $FILTER ] focus" ||
     st -e tmux new-session -A -s dropdown -c "$HOME"
 
