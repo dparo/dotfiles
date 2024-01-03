451b8629ffdfe04d6114ec160f825f57c3deeaeb
Davide Paro <dparo@outlook.it>
2024-01-03 20:32:44 +0100

    Fix dropdown terminal

--- roles/xdg/files/bin/i3-st-dropdown.sh
+++ roles/xdg/files/bin/i3-st-dropdown.sh
@@ -2,10 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # FILTER='instance="kitty" class="^dropdown-term$"'
-FILTER='instance="st-256color" class="^dropdown-term$"'
+FILTER='instance="st-256color|kitty|dropdown-term" class="^dropdown-term$"'
 
-# kitty --name "kitty" --class "dropdown-term" -e tmux new-session -A -s dropdown -c "$HOME"
+term() {
+    # exec kitty --name "kitty" --class "dropdown-term" --override 'initial_window_width=200c' --override 'initial_window_height=60c' -e tmux new-session -A -s dropdown -c "$HOME"
+    exec st -c "dropdown-term" -g 200x60 -e tmux new-session -A -s dropdown -c "$HOME"
+}
+
 i3-msg "[ $FILTER ] scratchpad show, move position center" ||
     i3-msg "[ $FILTER ] move workspace current, focus" ||
-    st -c "dropdown-term" -g 200x60 -e tmux new-session -A -s dropdown -c "$HOME"
+    term
 
