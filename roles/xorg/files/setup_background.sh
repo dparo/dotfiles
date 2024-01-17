9429fc6def7910fc872dd9537c0c873b2ebdbaac
Davide Paro <dparo@outlook.it>
2024-01-17 23:13:24 +0100

    Changed background theme

--- roles/xorg/files/setup_background.sh
+++ roles/xorg/files/setup_background.sh
@@ -14,6 +14,7 @@ set_wallpaper() {
 
 xsetroot -cursor_name left_ptr
 set_wallpaper \
+    /usr/share/backgrounds/gnome/adwaita-d.* \
     /usr/share/backgrounds/fedora-workstation/montclair_dark.webp \
     /usr/share/backgrounds/gnome/adwaita-l.* \
     /usr/share/backgrounds/f*/default/f*-01-night.png \
