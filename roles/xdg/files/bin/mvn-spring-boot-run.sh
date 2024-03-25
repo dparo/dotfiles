69b36281dd9ef88049b1adb6432e7bec7d58e858
Davide Paro <dparo@outlook.it>
2024-03-25 15:05:14 +0100

    Update

--- roles/xdg/files/bin/mvn-spring-boot-run.sh
+++ roles/xdg/files/bin/mvn-spring-boot-run.sh
@@ -9,4 +9,5 @@ JDWP_PORT=0         # USE 0 for dynamically determining a free port
 mvn -DcheckStyle.skip -DskipTests -Dmaven.test.skip \
     clean spring-boot:run \
     -Dspring-boot.run.arguments='--debug -Dspring.profiles.active=local' \
-    -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$JDWP_PORT"
+    -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$JDWP_PORT" \
+    "$@"
