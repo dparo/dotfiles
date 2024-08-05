9d22aeef04fa707a7b9ebefb6d151209c4149763
Davide Paro <dparo@outlook.it>
2024-08-05 14:40:33 +0200

    Fix mvn-spring-boot-run

--- roles/xdg/files/bin/mvn-spring-boot-run.sh
+++ roles/xdg/files/bin/mvn-spring-boot-run.sh
@@ -1,13 +1,39 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+set -eou pipefail
+
+OTHER_ARGS=()
+
+
+port="${JDWP_PORT:-5005}"
+suspend="n"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --debug-port)
+      port="$2"
+      shift
+      shift
+      ;;
+    --suspend)
+      suspend="s"
+      shift
+      ;;
+    -*|--*)
+      OTHER_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+    *)
+      OTHER_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
 
 set -x
-
-JDWP_PORT="${JDWP_PORT:-5005}"         # USE 0 for dynamically determining a free port
-
 mvn -DcheckStyle.skip -DskipTests -Dmaven.test.skip \
     clean spring-boot:run \
     -Dspring-boot.run.arguments='--debug -Dspring.profiles.active=local' \
-    -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$JDWP_PORT" \
-    "$@"
+    -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=$suspend,address=$port" \
+    "${OTHER_ARGS[@]}"
