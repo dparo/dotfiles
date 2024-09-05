bc0a6d42c9a47211ce281271f23c7331f739d514
Davide Paro <dparo@outlook.it>
2024-09-05 23:08:22 +0200

    feat(fish): Modfied config.

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,5 +1,10 @@
 set fish_greeting ""
 
+
+if status is-login
+    # Is login shell only
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
