4493b37b9340ac324f19416fa26204f6e9f2d887
Davide Paro <dparo@outlook.it>
2025-01-20 10:53:09 +0100

    Fix

--- roles/fish/files/functions/run.fish
+++ roles/fish/files/functions/run.fish
@@ -2,5 +2,5 @@
 status is-interactive; or exit 0
 
 function run --wraps=nohup --description 'alias run=nohup'
-  nohup $argv 1> /dev/null 2> /dev/null &
+ nohup $argv 1> /dev/null 2> /dev/null & disown;
 end
