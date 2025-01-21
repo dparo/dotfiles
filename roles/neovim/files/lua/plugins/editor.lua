91cb650e1f637f18cca8c681f0e9f683287665af
Davide Paro <dparo@outlook.it>
2025-01-21 20:01:36 +0100

    Nvim fix dashboard

--- roles/neovim/files/lua/plugins/editor.lua
+++ roles/neovim/files/lua/plugins/editor.lua
@@ -9,7 +9,7 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
       bigfile = { enabled = true },
-      dashboard = { enabled = true},
+      dashboard = { enabled = false },
       indent = { enabled = false },
       input = { enabled = true },
       notifier = { enabled = true },
