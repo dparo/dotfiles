8c5b6c41129166831575025fd28c33b08c2d9ff6
Davide Paro <dparo@outlook.it>
2025-04-29 21:34:19 +0200

    Added roles

--- cleanup.sh
+++ cleanup.sh
@@ -7,3 +7,32 @@ go clean -modcache
 go clean -cache
 
 npm cache clean --force
+
+# Load OS information
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+else
+    echo "Cannot determine OS. /etc/os-release not found."
+    exit 1
+fi
+
+# Check the distribution ID
+case "$ID" in
+    ubuntu)
+        echo "Running on Ubuntu"
+        ;;
+    arch)
+        if command -v yay >/dev/null 2>&1; then
+            yay -Scc
+        fi
+    ;;
+    fedora)
+        echo "Running on Fedora"
+        ;;
+    *)
+        echo "Running on an unsupported distribution: $ID"
+        ;;
+esac
+
+
+flatpak uninstall --unused
