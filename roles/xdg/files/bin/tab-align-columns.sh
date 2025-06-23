3f1abde5e86f0ce8cc92ad7a458b4cd6b6d2ed8d
Davide Paro <dparo@outlook.it>
2025-06-23 16:16:38 +0200

    Aggiornato script tab-align-columns.sh

--- roles/xdg/files/bin/tab-align-columns.sh
+++ roles/xdg/files/bin/tab-align-columns.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
 # Usage:
-#   tab-align-columns.sh [filename]
+#   tab-align-columns.sh [separator_regex] [filename]
+#   If no separator_regex is given, defaults to ' +' (one or more spaces).
+#   If no filename is given, reads from stdin.
 
-# If no filename is given, reads from stdin.
+SEP_REGEX=' +'
 
+# Check if the first argument looks like a regex or a file
 if [ $# -eq 0 ]; then
-    # No arguments; read from stdin
-    sed -E 's/ +/\t/g' | column -t -s $'\t'
+    # No args: use default sep, read from stdin
+    sed -E "s/${SEP_REGEX}/\t/g" | column -t -s $'\t'
 elif [ $# -eq 1 ]; then
-    # One argument; treat as filename
-    sed -E 's/ +/\t/g' "$1" | column -t -s $'\t'
+    # One arg: could be a file or separator
+    if [ -f "$1" ]; then
+        # It's a file
+        sed -E "s/${SEP_REGEX}/\t/g" "$1" | column -t -s $'\t'
+    else
+        # It's a custom separator
+        SEP_REGEX="$1"
+        sed -E "s/${SEP_REGEX}/\t/g" | column -t -s $'\t'
+    fi
+elif [ $# -eq 2 ]; then
+    SEP_REGEX="$1"
+    sed -E "s/${SEP_REGEX}/\t/g" "$2" | column -t -s $'\t'
 else
-    echo "Usage: $0 [filename]" >&2
+    echo "Usage: $0 [separator_regex] [filename]" >&2
     exit 1
 fi
-
