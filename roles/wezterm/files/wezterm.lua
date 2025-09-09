49e7bd481cdd703454900845a3d50874654cdba1
Davide Paro <dparo@outlook.it>
2025-09-09 21:42:07 +0200

    Fix wezterm config

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -4,17 +4,18 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+config.automatically_reload_config = false
 
--- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.default_prog = { "/usr/bin/fish" }
 
--- or, changing the font size and color scheme.
+
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "NONE"
+
 config.font_size = 10
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font = wezterm.font 'JetBrains Mono Nerd Font'
 config.color_scheme = 'tokyonight_storm'
 
--- Finally, return the configuration to wezterm:
 return config
 
