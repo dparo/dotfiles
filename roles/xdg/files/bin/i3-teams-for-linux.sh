d3b3b9aff5f3cc6d99bf1401e0fe3b760ae9c0a2
Davide Paro <dparo@outlook.it>
2025-09-11 12:24:33 +0200

    Fix switch teams

--- roles/xdg/files/bin/i3-teams-for-linux.sh
+++ roles/xdg/files/bin/i3-teams-for-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-FILTER='instance="teams-for-linux|microsoft teams" class="teams-for-linux|Microsoft Teams"'
+FILTER='instance="teams-for-linux|microsoft teams|crx_+cifhbcnohmdccbgoicgdjpfamggdegmo" class="teams-for-linux|Microsoft Teams|Google-chrome|Microsoft-edge|Brave-browser|Chromium-browser"'
 
 i3-msg "[ $FILTER ] scratchpad show, move position center" ||
     i3-msg "[ $FILTER ] focus" ||
