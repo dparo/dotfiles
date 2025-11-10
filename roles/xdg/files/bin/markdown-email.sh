f86984b275e32f01261373ae7407eabb176b2157
Davide Paro <dparo@outlook.it>
2025-11-10 11:50:46 +0100

    Markdown email script. Added custom template

--- roles/xdg/files/bin/markdown-email.sh
+++ roles/xdg/files/bin/markdown-email.sh
@@ -1,6 +1,8 @@
 # Create temporary CSS file
 TEMP_CSS="$(mktemp).css"
+TEMP_HTML_TEMPLATE="$(mktemp).html"
 trap "rm -f $TEMP_CSS" EXIT
+trap "rm -f $TEMP_HTML_TEMPLATE" EXIT
 
 cat > "$TEMP_CSS" << 'EOF'
 /*
@@ -237,6 +239,84 @@ pre code {
 }
 EOF
 
+# Output the default template with: `pandoc -D html5`
+cat > "$TEMP_HTML_TEMPLATE" << 'EOF'
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="$lang$" xml:lang="$lang$"$if(dir)$ dir="$dir$"$endif$>
+<head>
+  <meta charset="utf-8" />
+  <meta name="generator" content="pandoc" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+$for(author-meta)$
+  <meta name="author" content="$author-meta$" />
+$endfor$
+$if(date-meta)$
+  <meta name="dcterms.date" content="$date-meta$" />
+$endif$
+$if(keywords)$
+  <meta name="keywords" content="$for(keywords)$$keywords$$sep$, $endfor$" />
+$endif$
+$if(description-meta)$
+  <meta name="description" content="$description-meta$" />
+$endif$
+  <title>$if(title-prefix)$$title-prefix$ – $endif$$pagetitle$</title>
+  <style>
+    $styles.html()$
+  </style>
+$for(css)$
+  <link rel="stylesheet" href="$css$" />
+$endfor$
+$for(header-includes)$
+  $header-includes$
+$endfor$
+$if(math)$
+$if(mathjax)$
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+$endif$
+  $math$
+$endif$
+</head>
+<body>
+$for(include-before)$
+$include-before$
+$endfor$
+$if(title)$
+<header id="title-block-header">
+<h1 class="title">$title$</h1>
+$if(subtitle)$
+<p class="subtitle">$subtitle$</p>
+$endif$
+$for(author)$
+<p class="author">$author$</p>
+$endfor$
+$if(date)$
+<p class="date">$date$</p>
+$endif$
+$if(abstract)$
+<div class="abstract">
+<div class="abstract-title">$abstract-title$</div>
+$abstract$
+</div>
+$endif$
+</header>
+$endif$
+$if(toc)$
+<nav id="$idprefix$TOC" role="doc-toc">
+$if(toc-title)$
+<h2 id="$idprefix$toc-title">$toc-title$</h2>
+$endif$
+$table-of-contents$
+</nav>
+$endif$
+$body$
+$for(include-after)$
+$include-after$
+$endfor$
+</body>
+</html>
+EOF
+
+
 # Parse arguments
 CLIPBOARD=false
 INPUT="-"
@@ -263,9 +343,14 @@ HTML_OUTPUT=$(sed '/^---$/,/^---$/d' "${INPUT}" | pandoc \
     -s \
     --embed-resources \
     --metadata title=" " \
+    --metadata date-meta="$(date --iso-8601=minutes)" \
+    --metadata author-meta="$(git config get user.name) <$(git config get user.email)>" \
+    --metadata date="$(date --iso-8601=minutes)" \
+    --metadata author="$(git config get user.name) <$(git config get user.email)>" \
     --webtex='https://latex.codecogs.com/png.image?' \
     -V lang=it \
     --highlight-style=zenburn \
+     --template "$TEMP_HTML_TEMPLATE" \
      --css "$TEMP_CSS" \
     -f markdown+smart \
     --to=html5 \
