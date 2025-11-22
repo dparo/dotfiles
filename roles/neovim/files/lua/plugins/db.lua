7d3a1973b5f2e266bb7e313dfdea08d7ee8ddb7e
Davide Paro <dparo@outlook.it>
2025-11-22 14:20:37 +0100

    Nvim dadbod

--- roles/neovim/files/lua/plugins/db.lua
+++ roles/neovim/files/lua/plugins/db.lua
@@ -1,5 +1,21 @@
 return {
     { "tpope/vim-dadbod" },
-    { "kristijanhusak/vim-dadbod-ui", dependencies = { "tpope/vim-dadbod" } },
+     {
+      'kristijanhusak/vim-dadbod-ui',
+      dependencies = {
+        { 'tpope/vim-dadbod', lazy = true },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+      },
+      cmd = {
+        'DBUI',
+        'DBUIToggle',
+        'DBUIAddConnection',
+        'DBUIFindBuffer',
+      },
+      init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+      end,
+    },
     { "kristijanhusak/vim-dadbod-completion", dependencies = { "tpope/vim-dadbod", "vim-dadbod-completion" } },
 }
