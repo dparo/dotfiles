349fc9f5c475a2cafc3281964b6fd2a9e6804a98
Davide Paro <dparo@outlook.it>
2026-04-04 12:42:37 +0200

    fix(neovim): Renabled treesitter

--- roles/neovim/files/lua/user/autocommands.lua
+++ roles/neovim/files/lua/user/autocommands.lua
@@ -58,6 +58,34 @@ core.utils.augroup("USER_SKELETONS", {
     },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*" },
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if not vim.treesitter.language.add(lang) then
+      local available = vim.g.ts_available
+        or require("nvim-treesitter").get_available()
+      if not vim.g.ts_available then
+        vim.g.ts_available = available
+      end
+      if vim.tbl_contains(available, lang) then
+        require("nvim-treesitter").install(lang)
+      end
+    end
+
+    if vim.treesitter.language.add(lang) then
+      vim.treesitter.start(args.buf, lang)
+      -- folds, provided by Neovim
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo.foldmethod = 'expr'
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
+})
+
 -- Filetypes autocommand
 core.utils.augroup("USER_FILETYPES", {
     {
