dfdf8b994a5048a793d0e8a89be658654e355493
Davide Paro <dparo@outlook.it>
2026-07-27 21:43:33 +0200

    fix(fish): csv2md fix escape

--- roles/fish/files/functions/csv2md.fish
+++ roles/fish/files/functions/csv2md.fish
@@ -5,7 +5,18 @@ function csv2md
     import pandas as pd
     import sys
 
+    def escape_markdown_cell(x):
+        if not isinstance(x, str):
+            return x
+        return (
+            x.replace("\\", "\\\\")
+             .replace("|", "\\|")
+             .replace("\n", "\\n")      # or "<br>"
+        )
+
+
     df = pd.read_csv(sys.stdin, sep="\t", dtype=str, keep_default_na=False)
+    df = df.map(escape_markdown_cell)
     print(df.to_markdown(index=False))
     '
 end
