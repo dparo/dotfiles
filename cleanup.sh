1245b3a1b60c5e5bf8a9bf2b93807912c93e2d0e
Davide Paro <dparo@outlook.it>
2026-09-08 10:57:32 +0200

    feat(cleanup): Delete IntelliJ dirs

--- cleanup.sh
+++ cleanup.sh
@@ -30,6 +30,39 @@ case "$ID" in
 esac
 
 
+function cleanup_intellij_idea_old_dirs() {
+    keep=1
+    delete=false
+    [[ ${1:-} == "--delete" ]] && delete=true
+
+    root="${XDG_DATA_HOME:-$HOME/.local/share}/JetBrains"
+    [[ -d $root ]] || exit 0
+
+    mapfile -d '' -t dirs < <(
+        find "$root" -mindepth 1 -maxdepth 1 -type d \
+            -name 'IntelliJIdea[0-9]*' -print0 |
+        sort -zV
+    )
+
+    remove_count=$((${#dirs[@]} - keep))
+    ((remove_count > 0)) || exit 0
+
+    for ((i = 0; i < remove_count; i++)); do
+        dir=${dirs[i]}
+        name=${dir##*/}
+
+        # Extra guard against deleting unrelated directories.
+        [[ $name =~ ^IntelliJIdea[0-9]{4}\.[0-9]+([.][0-9]+)?$ ]] || continue
+
+        if $delete; then
+            echo "Deleting $dir"
+            rm -rf -- "$dir"
+        else
+            echo "Would delete $dir"
+        fi
+    done
+}
+
 
 set -x
 
@@ -64,4 +97,6 @@ rm -rf /home/dparo/.var/app/com.spotify.Client/cache
 # Delete nvim undo history, older than 90 days
 find /home/dparo/.cache/nvim/nvim/undo -type f -mtime +90 -delete
 #   Otherwise go for hard delete: rm -rf /home/dparo/.cache/nvim/nvim/undo/
+
+cleanup_intellij_idea_old_dirs --delete
 
